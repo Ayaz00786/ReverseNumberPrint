ab27929a736e1cda647247e31ac3eff3e2eb7c9e
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-24 02:11:20 +0530

    Add project files.

---
+++ ReverseNumberPrint/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReverseNumberPrint
+{
+    internal class Class1
+    {
+        public static void reverse()
+        {
+            Console.WriteLine("Enter a number:");
+            int number= int.Parse(Console.ReadLine());
+            int result = 0;
+
+            while (number > 0)
+            {
+                int rem = number % 10;
+                result = result * 10 + rem;
+                number =number/ 10;
+            }
+            Console.WriteLine("Reverse Number Is: {0}",result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReverseNumberPrint/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReverseNumberPrint
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.reverse();
+        }
+    }
+}
